1044e0f61a83a5661a5c943f72a4583ba30bf3a2
Krithikha Bala <59526292+krithikha2001@users.noreply.github.com>
2020-06-09 18:56:47 +0530

    Update ml.py

--- ml.py
+++ ml.py
@@ -13,8 +13,6 @@ for dirname, _, filenames in os.walk('/kaggle/input'):
     for filename in filenames:
         print(os.path.join(dirname, filename))
 
-# You can write up to 5GB to the current directory (/kaggle/working/) that gets preserved as output when you create a version using "Save & Run All" 
-# You can also write temporary files to /kaggle/temp/, but they won't be saved outside of the current session
 '''train = pd.read_csv("/kaggle/input/titanic/train.csv")
 train.head()
 x=train.loc[train.Sex=='female']
